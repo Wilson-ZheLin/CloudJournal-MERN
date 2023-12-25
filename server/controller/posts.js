7266baf045a645534feed5c3bc2cf3bdfbb3b3c7
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-25 02:34:53 -0500

    Bug Fixed: createPost API

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -10,7 +10,7 @@ export const getPosts = async (req, res) => {
 }
 
 export const createPost = async (req, res) => {
-    const post = req.post;
+    const post = req.body;
     const newPost = new PostMessage(post);
     try {
         await newPost.save();
