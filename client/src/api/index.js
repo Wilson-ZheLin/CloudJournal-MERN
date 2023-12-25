b3ec4b4475c5f43564f86dcffa698fcf86aeac8c
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-25 02:36:36 -0500

    Function Add: Create Post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,4 @@ import axios from "axios";
 const url = "http://localhost:3000/posts";
 
 export const fetchPosts = async () => await axios.get(url);
+export const createPost = async (newPost) => await axios.post(url, newPost);
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux";
 
 const Posts = () => {
     const posts = useSelector((state) => state.posts);
+    console.log(posts);
     return (
         <>
             <h1>Posts</h1>
