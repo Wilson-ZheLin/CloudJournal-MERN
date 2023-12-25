d2a84d1a7497fd6cd8d6a082ef6ff4dcb72369ac
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-25 02:35:50 -0500

    Bug Fixed: Compatibility optimization

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from "redux-thunk";
 
 import App from "./App";
 import reducers from "./reducers";
 
-const store = createStore(
-    reducers,
-    compose(applyMiddleware(thunk))
-)
+const store = configureStore({
+    reducer: reducers,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk)
+});
 
 const root = createRoot(document.getElementById("root"));
 root.render(
