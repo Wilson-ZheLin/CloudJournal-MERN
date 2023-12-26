b83081af6a2417313ef1d7c14f4f84ec26617b42
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-26 18:43:57 -0500

    Add Background

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { configureStore } from '@reduxjs/toolkit';
 import { thunk } from "redux-thunk";
 
+import "./index.css";
 import App from "./App";
 import reducers from "./reducers";
 
