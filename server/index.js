1e0ce5721f10149e5f24aca0419dac8ea6cb78cb
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-27 23:51:06 -0500

    Add Environment Info

--- server/index.js
+++ server/index.js
@@ -3,18 +3,19 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import postsRoutes from "./route/posts.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config();
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/posts", postsRoutes);
 
-const CONNECTION_URL = "";
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(CONNECTION_URL)
+mongoose.connect(process.env.CONNECTION_URL)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
