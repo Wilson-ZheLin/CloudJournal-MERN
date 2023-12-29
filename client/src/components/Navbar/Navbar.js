19dc8f81f5b83f8fb6ea69c176beb792d70c405d
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-29 00:02:37 -0500

    Function Add: Logout

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Typography from '@mui/material/Typography';
 import Toolbar from '@mui/material/Toolbar';
 import Avatar from '@mui/material/Avatar';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Button } from '@mui/material';
 import useStyles from "./styles";
 import memories from '../../images/memories.png';
 
 const Navbar = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    
+    useEffect(() => {
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location]);
 
-    const user = null;
+    console.log(user);
+
+    const logout = () => {
+        dispatch({ type: 'LOGOUT' });
+        setUser(null);
+        navigate('/');
+    };
 
     return (
         <AppBar className={classes.appBar} position="static" color="inherit">
@@ -20,11 +36,11 @@ const Navbar = () => {
                 <img className={classes.image} src={memories} alt="memories" height="60"/>
             </div>
             <Toolbar className={classes.toolbar}>
-                {user ? (
+                {user && user.result ? (
                     <div className={classes.profile}>
-                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+                        <Avatar className={classes.purple} alt={user.result.name} src={user.result.picture}>{user.result.name.charAt(0)}</Avatar>
                         <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                        <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                        <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                     </div>
                 ) : (
                     <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
