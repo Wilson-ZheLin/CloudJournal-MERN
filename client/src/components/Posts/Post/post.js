3114a2eebf827b239820e55a52191f313ef15a81
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-31 22:14:13 -0500

    Optimization: Reconstruct the Post

--- client/src/components/Posts/Post/post.js
+++ client/src/components/Posts/Post/post.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { Card, CardActions, CardContent, CardMedia, Button, Typography, CardActionArea } from '@mui/material';
-import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
-import ThumbUpAltOutlined from '@mui/icons-material/ThumbUpAltOutlined';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, CardActionArea, Icon } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
-import EditIcon from '@mui/icons-material/Edit';
+import Avatar from '@mui/material/Avatar';
+import CardHeader from '@mui/material/CardHeader';
 import moment from 'moment';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
+import { red, green, blue, yellow, purple } from '@mui/material/colors';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import IconButton from '@mui/material/IconButton';
 import useStyles from "./styles";
 import { deletePost, likePost } from '../../../actions/posts';
 
@@ -16,45 +19,63 @@ const Post = ({ post, setCurrentId }) => {
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const user = JSON.parse(localStorage.getItem('profile'));
+
     const Likes = () => {
-      if (post?.likes?.length > 0) {
-        return post.likes.find((like) => like === (user?.result?.email || user?.result?._id))
-          ? (
-            <><ThumbUpAltIcon fontSize="small" />&nbsp;{post.likes.length > 2 ? `You and ${post.likes.length - 1} others` : `${post.likes.length} like${post.likes.length > 1 ? 's' : ''}` }</>
-          ) : (
-            <><ThumbUpAltOutlined fontSize="small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Like' : 'Likes'}</>
-          );
-      }
-      return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Like</>;
+      const userId = user?.result?._id;
+      const userEmail = user?.result?.email;
+      const isLiked = post.likes.includes(userId) || post.likes.includes(userEmail);
+    
+      return (
+        <FavoriteIcon fontSize="small" sx={{ color: isLiked ? '#D32F2F' : 'inherit' }} />
+      );
     };
 
     const openPost = () => {
       navigate(`/posts/${post._id}`);
     }
 
+    const colors = [red[500], blue[500], green[500], yellow[500], purple[500]];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
     return (
-        <Card className={classes.card} raised elevation={4}>
-            <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'} title={post.title} />
-            <div className={classes.overlay}>
-              <Typography variant="h6">{post.name}</Typography>
-              <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-            </div>
-            {(user?.result?.email === post?.creator || user?.result?._id === post?.creator) && (
-              <div className={classes.overlay2}>
-                <Button style={{ color: 'white' }} size="large" onClick={() => setCurrentId(post._id)}><EditIcon fontSize="default" /></Button>
+        <Card className={classes.card} sx ={{ borderRadius: '8px' }} raised elevation={4}>
+            <CardHeader 
+              className={classes.cardHeader}
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  {post.creator?.charAt(0)?.toUpperCase()}
+                </Avatar>
+              }
+              action={
+                (user?.result?.email === post?.creator || user?.result?._id === post?.creator) ?
+                  <IconButton aria-label="settings" onClick={() => setCurrentId(post._id)}>
+                    <MoreVertIcon />
+                  </IconButton>
+                : null
+              }
+              title={post.name}
+              subheader={moment(post.createdAt).format('MMM DD, YYYY')}
+            />
+            <CardActionArea onClick={openPost}>
+              <CardMedia className={classes.media} image={post.selectedFile || 'https://user-images.githubusercontent.com/194400/49531010-48dad180-f8b1-11e8-8d89-1e61320e1d82.png'}/>
+              <CardContent>
+                <Typography gutterBottom variant="h6" component="h2">{post.title}</Typography>
+                <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
+              </CardContent>
+
+              <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
               </div>
-            )}
-            <div className={classes.details}>
-              <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
-            </div>
-            <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
-            <CardContent>
-              <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
-            </CardContent>
+            </CardActionArea>
+            
           <CardActions className={classes.cardActions}>
-            <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}><Likes /></Button>
+            
+            <div className={classes.like}>
+              <IconButton aria-label="add to favorites" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))} sx={{ ml: '0'}}><Likes /></IconButton>
+              <Typography variant="body2" color="textSecondary" component="h2">{post.likes.length}</Typography>
+            </div>
             {(user?.result?.email === post?.creator || user?.result?._id === post?.creator) && (
-              <Button size="small" color="inherit" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon fontSize="small" /> Delete</Button>
+              <IconButton size="small" className={classes.delete} aria-label="edit" onClick={() => dispatch(deletePost(post._id))}><DeleteIcon /></IconButton>
             )}
           </CardActions>
         </Card>
