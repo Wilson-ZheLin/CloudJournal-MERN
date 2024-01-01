79d479f05da03661d633b155c6589353c33b14a3
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2023-12-31 22:13:32 -0500

    Feature Add: User avatar

--- server/model/user.js
+++ server/model/user.js
@@ -12,7 +12,22 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    protrait: {
+        type: String,
+        required: false
+    },
+    color: {
+        type: Number,
+        required: true
     }
+});
+
+userSchema.pre('save', function(next) {
+    if (this.isNew) {
+        this.color = Math.floor(Math.random() * 5);
+    }
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
