c199ca24651cb105c614a449da42f38ee6608ce4
Wilson-ZheLin <lin.zhe3@northeastern.edu>
2024-01-07 19:38:01 -0500

    Feature Add: Rate Limit

--- server/index.js
+++ server/index.js
@@ -5,11 +5,21 @@ import mongoose from "mongoose";
 import postsRoutes from "./route/posts.js";
 import userRoutes from "./route/users.js";
 import dotenv from "dotenv";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 dotenv.config();
 
+const apiLimiter = rateLimit({
+   windowsMs: 15 * 60 * 1000,
+   max: 100,
+   message: "Too many requests from this IP, please try again after 15 minutes",
+   standardHeaders: true,
+   legacyHeaders: false,
+});
+
 app.use(cors());
+app.use('/posts', apiLimiter);
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use("/posts", postsRoutes);
